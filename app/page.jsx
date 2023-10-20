92125c56c824b06e800e8a0d89ca7dd77f891d6e
ashantys <ashantys.m@gmail.com>
2023-10-20 13:40:12 -0600

    fix (scroll-to Se arreglo el scroll para el servidor

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,4 @@
-'use client';
 import 'bootstrap/dist/js/bootstrap.min.js'
-import Image from 'next/image';
 import styles from './page.module.css';
 import '@/styles/style.css';
 import Navbar from "@/components/Navbar";
