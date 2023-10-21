0829b4c2830a0caa82578b724b3eebc0f3541641
ashantys <ashantys.m@gmail.com>
2023-10-20 18:26:46 -0600

    fix

--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,11 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.min.js';
 import styles from './page.module.css';
 import '@/styles/style.css';
-import Navbar from "@/components/Navbar";
+
 
 
 export default function Home() {
   return (
     <>
-
     </>
   )
 }
