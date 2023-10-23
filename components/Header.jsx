62f71b6e6f3543fd5d66ef90c1569f60657bfd21
ashantys <ashantys.m@gmail.com>
2023-10-23 11:20:11 -0600

    feat(Header):se agrego el componente Header

---
+++ components/Header.jsx
@@ -0,0 +1,27 @@
+'use client';
+import React from 'react';
+
+const Header = () =>{
+    return(
+        <div className="background-welcome">
+        <div className="welcome-container">
+                <h1>
+                    ¡Invierte en el Futuro con Kadiz!
+                </h1>
+                <p>
+                    Kadiz es tu socio de confianza para llevar
+                    a cabo proyectos de desarrollo de fraccionamientos. 
+                    Nuestra experiencia y pasión en la construcción de 
+                    comunidades exitosas nos convierten en la elección 
+                    perfecta para inversionistas como tú.
+                </p>
+                    <a href="#contacto" className="btn btn-primary" role="button">Comencemos el Éxito</a>
+
+        </div>
+      </div>
+    );
+
+
+};
+
+export default Header;
