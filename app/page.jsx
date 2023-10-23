699c06060cbfe840ad9400074dbc2cc684b29f58
ashantys <ashantys.m@gmail.com>
2023-10-23 11:39:02 -0600

    feat(header):se agrego header sin imagen

--- app/page.jsx
+++ app/page.jsx
@@ -4,11 +4,9 @@ import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
 
 
-
 export default function Home() {
   return (
     <>
-      <Navbar/>
       <Header/>
     </>
   )
