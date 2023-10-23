ed3c99e2ea57780fd0c3b0521f386a6039f685af
ashantys <ashantys.m@gmail.com>
2023-10-23 11:01:09 -0600

    feat(boostrap):se agrego boostrap al proyecto

--- app/page.jsx
+++ app/page.jsx
@@ -1,11 +1,13 @@
 import styles from './page.module.css';
 import '@/styles/style.css';
+import Navbar from '@/components/Navbar';
 
 
 
 export default function Home() {
   return (
     <>
+      <Navbar/>
     </>
   )
 }
