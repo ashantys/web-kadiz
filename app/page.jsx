ef7f0cdf05c10f45411469ea3b5499ea45625b64
ashantys <ashantys.m@gmail.com>
2023-10-23 13:56:54 -0600

    feat(icon):se agrego icon

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import styles from './page.module.css';
 import '@/styles/style.css';
+import Head from 'next/head';
 import Navbar from '@/components/Navbar';
 import Header from '@/components/Header';
 import Nosotros from '@/components/Nosotros';
@@ -11,12 +12,19 @@ import Footer from '@/components/Footer';
 export default function Home() {
   return (
     <>
-      <Navbar/>
-      <Header/>
-      <Nosotros/>
-      <Servicios/>
-      <Contacto/>
-      <Footer/>
+      <Head>
+        <link rel="icon" href="/images/logo-icono.svg"/>
+        <title>Kadiz Ineniería</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </Head>
+
+          <Navbar/>
+          <Header/>
+          <Nosotros/>
+          <Servicios/>
+          <Contacto/>
+          <Footer/>
+
     </>
   )
 }
